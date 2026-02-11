7270eddd746115b81f119482fd3fed6a081e5ca5
Jannat63 <ajbmix63@gmail.com>
2026-02-11 20:00:31 +0600

    Added Eleventy blog structure

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("style.css");
+  eleventyConfig.addPassthroughCopy("images");
+  
+  return {
+    dir: {
+      input: ".",
+      output: "_site"
+    }
+  };
+};
