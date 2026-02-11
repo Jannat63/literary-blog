d0a53dab5349740e6021e898ee93ed7555e9cd9c
Jannat63 <ajbmix63@gmail.com>
2026-02-11 22:33:21 +0600

    Page added

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,18 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("admin");
 
+  eleventyConfig.addCollection("poems", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("content/poems/*.md");
+  });
+
+  eleventyConfig.addCollection("stories", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("content/stories/*.md");
+  });
+
+  eleventyConfig.addCollection("books", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("content/books/*.md");
+  });
+
   return {
     dir: {
       input: ".",
