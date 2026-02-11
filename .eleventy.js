ed79d1545cd0b0eeeb470c8028c166d5a25fc91d
Jannat63 <ajbmix63@gmail.com>
2026-02-11 22:14:50 +0600

    Testing foult

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("style.css");
-  eleventyConfig.addPassthroughCopy("images");
-  
+
+  eleventyConfig.addPassthroughCopy("admin");
+
   return {
     dir: {
       input: ".",
